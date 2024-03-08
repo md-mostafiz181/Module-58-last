ee9e87fd8cadc105bedf38c33b47d963a3c1698e
mostafizur rahman <dev.mostafizurrahman@gmail.com>
2024-03-08 08:57:36 +0600

    added class no-02 nested route

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <a href="/">Home</a>
+            <a href="contact">Contact</a>
+            <a href="about">About</a>
+            
+        </div>
+    );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -7,21 +7,42 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Home from './components/Home/Home.jsx';
+import Header from './components/Header/Header.jsx';
+import Contact from './components/Contact/Contact.jsx';
+import About from './components/About/About.jsx';
 
-const router = createBrowserRouter([
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <div>This is Mostafizur Rahman</div>,
+//   },
+//   {
+//     path:"/about",
+//     element: <div>This is about page</div>,
+//   },
+//   {
+//     path:"/contact",
+//     element:<div>This is contact page</div>
+//   }
+// ]);
+
+const router=createBrowserRouter([
   {
-    path: "/",
-    element: <div>This is Mostafizur Rahman</div>,
-  },
-  {
-    path:"/about",
-    element: <div>This is about page</div>,
-  },
-  {
-    path:"/contact",
-    element:<div>This is contact page</div>
+    path:"/",
+    element:<Home></Home>,
+    children:[
+      {
+        path:"/contact",
+        element:<Contact></Contact>,
+      },
+      {
+        path:"/about",
+        element:<About></About>
+      }
+    ]
   }
-]);
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
